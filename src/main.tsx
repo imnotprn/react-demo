884995429b01a8c5d02a6bbe0d44a9f9ab9c7db4
Macbook <macbook@Macbooks-MacBook-Air.local>
2023-10-06 14:01:25 -0700

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import AuthProvider from "./providers/Authprovider.tsx";
+import AuthProvider from "./providers/AuthProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
